be66473bd479d9fcbb71fc2763ea3c5087a63545
Sidhil Sivadas Manoli (Trianz) <sidhilsivadas.m@trianz.com>
2023-09-06 14:51:17 +0530

    Added templates

--- app/index.js
+++ app/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const path = require('path');
 const friendsRouter = require("./routes/friends.routes")
 const app = express();
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/site', express.static(path.join(__dirname, '..','public')));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index', {
+      title: 'My Friends Are VERYY Clever',
+      caption: 'Let\'s go skiing!',
+    });
+  });
 
 
 app.use("/api/friends", friendsRouter)
